259dea479dc49e6b0812b3d9e3310b4a19ba2d75
Korhan Citlak <kcitlak1@cs.binghamton.edu>
2020-05-02 16:49:15 -0400

    Fixed insertion sort

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <chrono>
 #include <thread>
+#include <iostream>
+
 
 #include "data_sorting.h"
 
@@ -19,7 +21,7 @@ int main() {
 	//random_number_file_generator("dataset3_", 10, 20, 5);
 
 	
-        std::ifstream file("dataset3_10.dat");
+    std::ifstream file("dataset3_10.dat");
 	std::vector<int> data;
     int value;
     while(file >> value){
@@ -29,7 +31,7 @@ int main() {
 
 	//std::chrono::milliseconds timespan(1000);
 	//std::this_thread::sleep_for(timespan);
-	std::vector<long long> shell_sort_results = quick_sort(data);
+	std::vector<long long> shell_sort_results = insertion_sort(data);
 	cout << "PASSED" << endl;
 	for(int i = 0; i < shell_sort_results.size(); i++){
 		cout << shell_sort_results[i] << endl;
