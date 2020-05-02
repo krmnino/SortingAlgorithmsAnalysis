4a9c0e777d905b8dbab8956f2ee84f7d2e3e2f63
Ethan Breban <ebreban1@cs.binghamton.edu>
2020-05-02 16:53:47 -0400

    quicksort fixed

--- src/quick_sort.cpp
+++ src/quick_sort.cpp
@@ -3,13 +3,7 @@
 #include <vector>
 
 #include "data_sorting.h"
-
-static void swap(std::vector<int> a, int i, int j){
-  int t = a[i];
-  a[i] = a[j];
-  a[j] = t;
-}
-static int findPivot(std::vector<int> a, int i, int j){
+static int findPivot(std::vector<int>& a, int i, int j){
   return (i+j)/2;
 }
 
@@ -17,43 +11,47 @@ static int findPivot(std::vector<int> a, int i, int j){
  *  right sub-array, returning index of first position in right
  *  sub-array
  */
-static int partition(std::vector<int> a, int lo, int hi, int pivot, std::vector<long long> results){
-  std::cout << lo << std::endl;
-  std::cout << hi << std::endl;
-  while (lo <= hi) { //while interval is non-empty
-    while (a[lo] < pivot) {
-      //this loop will not run off the end of the array
-      //because pivot is at a[hi]
-      ++lo;
-    }
-     std::cout << "lo " << lo << std::endl;
-    while ((hi >= lo) && (a[hi] >= pivot)) --hi;
-	std::cout << "hi" << hi << std::endl; 
-     results[1]++;
-    if (hi > lo) swap(a, lo, hi); //swap out-of-place values
-    break;
-  }
-  return lo;            // Return first position in right partition
+static int partition(std::vector<int>& a, int lo, int hi, std::vector<long long>& results){
+	int pivot = a[hi];	
+	int i = (lo - 1); // Index of smaller element  
+  
+    for (int j = lo; j <= hi - 1; j++)  
+    {  
+        // If current element is smaller than the pivot  
+		results[1]++;
+        if (a[j] < pivot)  
+        {  
+            i++; // increment index of smaller element  
+			int t = a[i];  
+    		a[i] = a[j];  
+    		a[j] = t;
+			results[0]++; 
+        }  
+    }  
+ 
+	int t = a[i + 1];  
+    a[i + 1] = a[hi];  
+    a[hi] = t;
+	results[0]++;
+    return (i + 1);    
+	
 }
 
 /** sort a [lo, hi] */
-static void qsort(std::vector<int> a, int lo, int hi, std::vector<long long> results){
- 
-  if (hi - lo < 1) return;
-  int pivotindex = findPivot(a, lo, hi);  // Pick a pivot
-  swap(a, pivotindex, hi);
-  results[0]++;           // Stick pivot at end
-  // k will be the first position in the right subarray
+static void qsort(std::vector<int>& a, int lo, int hi, std::vector<long long>& results){
+  if (lo < hi)  
+    {  
+        /* pi is partitioning index, arr[p] is now  
+        at right place */
+        int pi = partition(a, lo, hi, results);  
   
-  int k = partition(a, lo, hi-1, a[hi], results);
-
-  swap(a, k, hi);
-  results[0]++;                         // Put pivot in place
-  qsort(a, lo, k - 1, results);  // Sort left partition
-  qsort(a, k + 1, hi, results);  // Sort right partition
-  
+        // Separately sort elements before  
+        // partition and after partition  
+        qsort(a, lo, pi - 1, results);  
+        qsort(a, pi + 1, hi, results);  
+    }  
 }
-
+  
 /** sort a [0, n) */
 std::vector<long long> quick_sort(std::vector<int> a){
   int n = a.size();
@@ -65,5 +63,10 @@ std::vector<long long> quick_sort(std::vector<int> a){
   std::chrono::duration<double> delta_time = finish - begin;
   //Time in milliseconds
   results[2] = delta_time.count() * 1000;
+  n = sizeof(a) / sizeof(a[0]);  
+  std::cout << "Sorted array: \n";  
+  for (int i = 0; i < a.size(); i++)  
+  std::cout << a[i] << " ";  
+  std::cout << std::endl;  
   return results;
 }
