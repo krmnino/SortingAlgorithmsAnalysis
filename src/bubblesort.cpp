62e4dd0a4d8edbdc143bacb5594c4b386d63ea14
Ethan Breban <ethanbreban@Ethans-MacBook-Air.local>
2020-05-02 14:13:21 -0400

    bubble sort added and quicksort changed

---
+++ src/bubblesort.cpp
@@ -0,0 +1,31 @@
+#include <chrono>
+#include <vector>
+
+#include "data_sorting.h"
+
+static void swap(std::vector<int> a, int i, int j){
+  int t = a[i];
+  a[i] = a[j];
+  a[j] = t;
+}
+std::vector<long long> bubbleSort(std::vector<int> a){
+  int n = a.size();
+  //index 0 = swaps; index 1 = comparisons; index 2 = time;
+  std::vector<long long> results;
+  results.resize(3);
+  auto begin = std::chrono::system_clock::now();
+  for (int i = 0; i < n - 1; ++i) {
+    for (int j = n - 1; j > i; --j) {
+      results[1]++;
+      if (a[j] < a[j - 1]){
+        swap(a, j, j - 1);
+        results[0]++;
+      }
+    }
+  }
+  auto finish = std::chrono::system_clock::now();
+	std::chrono::duration<double> delta_time = finish - begin;
+  //Time in milliseconds
+  results[2] = delta_time.count() * 1000;
+  return results;
+}
--- src/quicksort.cpp
+++ src/quicksort.cpp
@@ -1,12 +1,15 @@
 
-swap(int a[], int i, int j)
-{
+#include <chrono>
+#include <vector>
+
+#include "data_sorting.h"
+
+static void swap(std::vector<int> a, int i, int j){
   int t = a[i];
   a[i] = a[j];
   a[j] = t;
 }
-static int findPivot(int a[], int i, int j)
-{
+static int findPivot(std::vector<int> a, int i, int j){
   return (i+j)/2;
 }
 
@@ -14,8 +17,7 @@ static int findPivot(int a[], int i, int j)
  *  right sub-array, returning index of first position in right
  *  sub-array
  */
-static int partition(int a[], int lo, int hi, int pivot)
-{
+static int partition(int a[], int lo, int hi, int pivot){
   while (lo <= hi) { //while interval is non-empty
     while (a[lo] < pivot) {
       //this loop will not run off the end of the array
@@ -29,8 +31,7 @@ static int partition(int a[], int lo, int hi, int pivot)
 }
 
 /** sort a [lo, hi] */
-static void qsort(int a[], int lo, int hi)
-{
+static void qsort(std::vector<int> a, int lo, int hi){
   if (hi - lo < 1) return;
   int pivotindex = findPivot(a, lo, hi);  // Pick a pivot
   swap(a, pivotindex, hi);           // Stick pivot at end
@@ -39,10 +40,19 @@ static void qsort(int a[], int lo, int hi)
   swap(a, k, hi);                        // Put pivot in place
   qsort(a, lo, k - 1);  // Sort left partition
   qsort(a, k + 1, hi);  // Sort right partition
+
 }
 
 /** sort a [0, n) */
-void quickSort(int a[], int n)
-{
-  qsort(a, 0, n - 1);
+std::vector<long long> quickSort(std::vector<int> a){
+  int n = a.size();
+  std::vector<long long> results;
+  results.resize(3);
+  auto begin = std::chrono::system_clock::now();
+  qsort(a, 0, n - 1, begin);
+  auto finish = std::chrono::system_clock::now();
+	std::chrono::duration<double> delta_time = finish - begin;
+  //Time in milliseconds
+  results[2] = delta_time.count() * 1000;
+  return results;
 }
