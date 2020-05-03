f26ee2405cce00694ddcee517e5baec8b8b394d8
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2020-05-02 21:01:53 -0400

    implemented command recognition for cmd functionality

--- src/bubble_sort.cpp
+++ src/bubble_sort.cpp
@@ -4,13 +4,13 @@
 #include "data_sorting.h"
 
 std::vector<long long> bubble_sort(std::vector<int> a){
-  int n = a.size();
+  //int n = a.size();
   //index 0 = swaps; index 1 = comparisons; index 2 = time;
   std::vector<long long> results;
   results.resize(3);
   auto begin = std::chrono::system_clock::now();
-  for (int i = 0; i < n - 1; ++i) {
-    for (int j = n - 1; j > i; --j) {
+  for (int i = 0; i < a.size() - 1; ++i) {
+    for (int j = a.size() - 1; j > i; --j) {
       results[1]++;
       if (a[j] < a[j - 1]){
           int t = a[j];
--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,66 @@
 
 using namespace std;
 
+static bool command_exists(string commands[], int size, string command){
+	for(int i = 0; i < size; i++){
+		if(command == commands[i]){
+			return true;
+		}
+	}
+	return false;
+}
+
+static vector<string> split_string(string input){
+	vector<string> out;
+	int end_space = input.find(' ');
+	int end_semicolon = input.find(';');
+	if(end_semicolon == -1){
+		return out;
+	}
+	while(true){
+		if(end_space == -1 && end_semicolon != -1){
+			out.push_back(input.substr(0, end_semicolon));
+			break;
+		}
+		out.push_back(input.substr(0, input.find(' ')));
+		input = input.substr(input.find(' ') + 1, input.length());
+		end_space = input.find(' ');
+		end_semicolon = input.find(';');
+		if(end_space == -1 && end_semicolon == -1){
+			break;
+		}
+	}
+}
+
+static void command_line(){
+	string commands[] = {"generate", "all", "show", "help", "set_path", "exit"};
+	string input = "";
+	vector<string> parsed_string;
+	while(true){
+		cout << ">> ";
+		getline(std::cin, input);
+		parsed_string = split_string(input);
+		if(parsed_string.size() == 0){
+			cout << "Missing ';' at end of line. Try again." << endl;
+			continue;
+		}
+		if(!command_exists(commands,sizeof(commands)/sizeof(commands[0]),parsed_string[0])){
+			cout << "Invalid input. Type 'help' to open manual." << endl;
+			continue;
+		}
+	}
+	
+}
+
 int main() {
+	/*
 	/*int i = 1000;
 	while (i < 10000001) {
 		ofstream out_file("dataset_" + to_string(i) + ".dat");
 		random_number_generator(out_file, i);
 		out_file.close();
 		i *= 10;
-	}*/
+	}
 	//random_number_file_generator("dataset3_", 10, 20, 5);
 
 	
@@ -39,4 +91,6 @@ int main() {
 	for(int i = 0; i < shell_sort_results.size(); i++){
 		cout << shell_sort_results[i] << endl;
 	}
+	*/
+	command_line();
 }
