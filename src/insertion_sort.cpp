8ea54c4eea1303c3a133e504429e3674a957f2bf
Kurt Manrique Nino <kmanriq1@cs.binghamton.edu>
2020-05-04 17:01:00 -0400

    fixed insertion algorithm

--- src/insertion_sort.cpp
+++ src/insertion_sort.cpp
@@ -10,24 +10,29 @@
 std::vector<long long>
 insertion_sort(std::vector<int> data)
 {
-   std::vector<long long> results;
-   results.resize(3);
-   auto begin = std::chrono::system_clock::now();
-  for (int i = 1; i < data.size(); ++i) {
-    //insert i'th record into sorted portion
-    for (int j = i; (j > 0) && (data[j] < data[j - 1]); --j) {
-      
-	  int x = data[j];
-      data[i] = data[j-1];
-      data[j-1] = x;
-	  results[0]++;
-	  results[1]++;
-    }
-  }
-  auto finish = std::chrono::system_clock::now();
-  std::chrono::duration<double> delta_time = finish - begin;
-  //Time in milliseconds
-  results[2] = delta_time.count() * 1000;
-  return results;
+	std::vector<long long> results;
+	results.resize(3);
+	auto begin = std::chrono::system_clock::now();
+
+	for(int i = 1; i < data.size(); i++)
+		{
+			for(int j = i; j > 0; j--)
+			{
+				results[1]++;
+				if(data[j] < data[j-1])
+				{
+					int temp = data[j];
+					data[j] = data[j-1];
+					data[j-1] = temp;
+					results[0]++;
+				}
+			}
+		}
+
+	auto finish = std::chrono::system_clock::now();
+ 	std::chrono::duration<double> delta_time = finish - begin;
+ 	//Time in milliseconds
+	results[2] = delta_time.count() * 1000;
+	return results;
 }
 
