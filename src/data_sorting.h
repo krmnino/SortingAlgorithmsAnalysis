6a28231730fa09ca267cd7258543607a8ce129cd
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2020-05-04 00:00:15 -0400

    added command_line() fn def in header

--- src/data_sorting.h
+++ src/data_sorting.h
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <vector>
 
+void command_line();
 void random_number_generator(std::ofstream&, int);
 void random_number_file_generator(std::string, unsigned long long, unsigned long long, int);
 std::vector<long long> bubble_sort(std::vector<int>);
