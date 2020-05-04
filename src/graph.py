8d931544dd469a1b821b8a6256018feb907872bc
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2020-05-03 21:44:43 -0400

    implemented graphing utility

--- src/graph.py
+++ src/graph.py
@@ -2,13 +2,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
-print(sys.argv[0])
-print(sys.argv[1])
-print(sys.argv[2])
 
-def pl():
-	plt.plot([1000,10000], [0,3])
-	plt.title("fp")
-	plt.xlabel("Input Size")
-	plt.ylabel("Time(ms)")
-	plt.show()
+def parse_file():
+    size = np.array([])
+    swaps = np.array([])
+    comps = np.array([])
+    time = np.array([])
+    input_data = open(str(parse_file))
+    for i, line in enumerate(input_data):
+        if(line == '\n'):
+            break
+        size = np.append(size, int(line.split(',')[0]))
+        swaps = np.append(swaps, int(line.split(',')[1]))   
+        comps = np.append(comps, int(line.split(',')[2]))
+        time = np.append(swaps, int(line.split(',')[3]))
+    return (size, swaps, comps, time)
+
+def plot_graph():
+    plt
+
+sort_algo = sys.argv[1]
+path = sys.argv[2]
+parse_file(path)
