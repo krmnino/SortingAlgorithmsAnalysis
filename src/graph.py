33553929068768a13cad41fb3a7e06f96de4bc6d
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2020-05-03 20:26:11 -0400

    implemented 'plot_res' and 'plot_all_res' for python plotting utility

--- src/graph.py
+++ src/graph.py
@@ -1,9 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
 
+print(sys.argv[0])
+print(sys.argv[1])
+print(sys.argv[2])
 
 def pl():
-	
 	plt.plot([1000,10000], [0,3])
 	plt.title("fp")
 	plt.xlabel("Input Size")
